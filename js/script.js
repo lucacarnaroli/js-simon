6c2f0ffa5dd7764f08f00fd4a613c04ca09bf671
luca <luca.carnaroli98@gmail.com>
2020-01-21 15:55:12 +0100

    prompt

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+
+
+setTimeout(bannerNum, 2000);
+var banner = alert('1 32 45 6 8 9 10');
+var array = [];
+function bannerNum() {
+  for (var i = 0; i < 5; i++) {
+    var putNum = prompt('Inserisci un numero alla volta');
+    if (putNum == banner) {
+      array.push(putNum);
+    }
+  }
+}
+clearTimeout(bannerNum);
+console.log(array);
